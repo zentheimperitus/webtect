57852ec7999c48f5460e9eb9d8f65872fd520abb
THEA ZEN MAMAR GUINALON <2015180@cc.usls.edu>
2025-02-11 10:51:10 +0800

    Hello World

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 Route::get('/login','App\Http\Controllers\LoginController@index');
 Route::post('/login','App\Http\Controllers\LoginController@auth');
 Route::get('/inventory','App\Http\Controllers\InventoryController@index');
+Route::get('/hello','App\Http\Controllers\HelloController@index');
