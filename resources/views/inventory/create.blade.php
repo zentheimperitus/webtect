d2671862759e27bfb9a2fd949a87cd4d8747beb4
THEA ZEN MAMAR GUINALON <2015180@cc.usls.edu>
2025-02-13 11:14:55 +0800

    Feat: Creating item add form

---
+++ resources/views/inventory/create.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <title>Document</title>
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="row">
+        <div class="card">
+          <br>
+          <div class="card=body">
+            <button type="Back" class="btn btn-primary">BACK</button>
+            <br>
+            <br>
+
+            <div class="mb-3">
+              <div class="form-group">
+                <label for="exampleInputPassword1">Categories:</label>
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>Select Category</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+    
+    
+            </div>
+            <div class="mb-3">
+              <div class="form-group">
+                <label for="exampleInputPassword1">Item name:</label>
+                <input type="item name" class="form-control" id="exampleInputPassword1" placeholder="Item Name">
+              </div>
+    
+    
+            </div>
+            <div class="mb-3">
+              <div class="form-group">
+                <label for="exampleInputPassword1">Price</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Price">
+              </div>
+              <div class="mb-3">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Quantity</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Quantity">
+                </div>
+                <br>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                
+    
+    
+            </div>
+
+          </div>
+        </div>
+          
+        
+
+
+
+      </div>
+
+
+    </div>
+
+  </body>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"
+  ></script>
+</html>
+
+
+      
+      
+      
